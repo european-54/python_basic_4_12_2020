fb48b2884a16933e831ba7dbb2939e103a414b0d
european-54 <artik-54@mail.ru>
2020-12-15 21:53:13 +0700

    second commit

--- homeworks/les_3/task1.py
+++ homeworks/les_3/task1.py
@@ -3,15 +3,25 @@
 у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
 
+def div(*args):
 
-def exe_1(a, b):
     try:
-        return a / b
-    except ZeroDivisionError:
-        return 'No / 0'
+        arg1 = int(input("Введите числитель "))
+        arg2 = int(input("Введите знаменатель "))
+        res = arg1 / arg2
     except ValueError:
-        return 'No value'
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Делить на ноль нельзя!"
+
+    return res
 
 
-def exe_1_use():
-    print(exe_1((int(input('Enter first number: '))), (int(input('Enter second number: ')))))
+#   if arg2 != 0:
+#       return arg1 / arg2
+#   else:
+#       print("Ошибка ! Делитель не может быть равен нулю")
+
+
+
+print(f'result  {div()}')
--- homeworks/les_3/task3.py
+++ homeworks/les_3/task3.py
@@ -2,17 +2,7 @@
 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших
 двух аргументов.
 """
-"""
 
-def my_func(a, b, c):
-    z = [a, b, c]
-    z.remove(min(a, b, c))
-    return sum(z)
-
-def my_func():
-print(my_func(4, 1, 9), (4, 1, 9), (4, 1, 9))
-
-"""
 def my_func(arg1 , arg2, arg3):
     if arg1 >= arg3 and arg2 >= arg3:
         return arg1 + arg2
@@ -21,9 +11,4 @@ def my_func(arg1 , arg2, arg3):
     else:
         return arg2 + arg3
 
-print(f'Result - {my_func(int(input("enter first argument ")), int(input("enter second argument ")), int(input("enter third argument ")))}')
-
-
-
-
-
+print(f'Result - {my_func(int(input("введите первый аргумент ")), int(input("введите второй аргумент ")), int(input("введите третий аргумент ")))}')
