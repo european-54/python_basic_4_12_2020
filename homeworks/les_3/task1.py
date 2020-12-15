e5a0c8e7d928cda90d3ea4e89d2d4cdf55cb560a
european-54 <artik-54@mail.ru>
2020-12-15 21:16:55 +0700

    second commit

--- homeworks/les_3/task1.py
+++ homeworks/les_3/task1.py
@@ -2,3 +2,16 @@
 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление. Числа запрашивать
 у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
+
+
+def exe_1(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 'No / 0'
+    except ValueError:
+        return 'No value'
+
+
+def exe_1_use():
+    print(exe_1((int(input('Enter first number: '))), (int(input('Enter second number: ')))))
--- homeworks/les_3/task2.py
+++ homeworks/les_3/task2.py
@@ -3,3 +3,16 @@
  город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы. Реализовать вывод
  данных о пользователе одной строкой.
 """
+
+
+def exe_2(**kwargs):
+    return list(kwargs.values())
+
+
+def exe_2_use():
+    print(exe_2(name=input('Enter name: '),
+                s_name=input('Enter second name: '),
+                b_date=input('Enter birth day: '),
+                l_town=input('Enter live town: '),
+                email=input('Enter email: '),
+                tel=input('Enter tel number: ')))
