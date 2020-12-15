cfa0229338bfb69c270c978f25f229224634b738
european-54 <artik-54@mail.ru>
2020-12-15 21:23:54 +0700

    second commit

--- homeworks/les_3/task2.py
+++ homeworks/les_3/task2.py
@@ -16,3 +16,14 @@ def exe_2_use():
                 l_town=input('Enter live town: '),
                 email=input('Enter email: '),
                 tel=input('Enter tel number: ')))
+
+def my_func(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, year, city, email, telephone])
+name = input('enter name')
+surname = input('enter surname')
+year = int(input('enter year'))
+city = input('enter city')
+email = input('enter email')
+telephone = input('input telephone')
+
+print(my_func(surname = 'Frolov', name = 'Sergey', year = '1990', city = 'Syzran', email = 'error@mail.ru', telephone = '8-903-300-99-87'))
