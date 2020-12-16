5fa63d4e1d63883e6176e10ad5b94ed81b504b41
european-54 <artik-54@mail.ru>
2020-12-16 13:23:19 +0700

    second commit

--- homeworks/les_4/script_params_test.py
+++ homeworks/les_4/script_params_test.py
@@ -1,13 +1,1 @@
-from sys import argv
-try:
-    script_name, time, salary, bonus = argv
-    print("Параметр 1: ", script_name)
-    print("Параметр 2: ", time)
-    print("Параметр 3: ", salary)
-    print("Параметр 4: ", bonus)
-    res = time * salary + bonus
-    print(f'заработная плата сотрудника {res}')
-except TypeError:
-    print('Ошибка ввода, введите числовое значение!')
-else:
-    print('Успех, нет ошибок!')
+
