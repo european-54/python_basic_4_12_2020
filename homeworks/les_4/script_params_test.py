16bcdc55847fc5fd5522c87c75b226ba7c103058
european-54 <artik-54@mail.ru>
2020-12-16 09:35:14 +0700

    second commit

--- homeworks/les_4/script_params_test.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-script_name, first_param, second_param, third_param = argv
-
-print("Имя скрипта: ", script_params_test.py)
-print("Параметр 1: ", first_param)
-print("Параметр 2: ", second_param)
-print("Параметр 3: ", third_param)
-
--- homeworks/les_4/task2.py
+++ homeworks/les_4/task2.py
@@ -5,3 +5,9 @@
 Результат: [12, 44, 4, 10, 78, 123].
 """
 
+
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
+new_list.pop(0)
+print(f"Исходный список: {my_list}")
+print(f"Новый список: {new_list}")
