43cc684dd610be423550d63e627e09a297942b69
- <you@example.com>
2020-12-25 09:29:08 +0700

    second commit

--- task2.py
+++ task2.py
@@ -4,3 +4,17 @@
 аргументы. Реализовать вывод данных о пользователе одной строкой.
 """
 
+name = str(input('enter name'))
+surname = str(input('enter surname'))
+year = int(input('enter year'))
+city = str(input('enter city'))
+email = str(input('enter email'))
+telephone =int(input('input telephone'))
+
+
+def my_func(*args):
+    args = (name, surname, year, city, email, telephone)
+    return args
+
+
+print(my_func('name', 'surname', year, 'city', 'email', telephone))
