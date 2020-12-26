53cabd175f6475b549df6bc9c7526fb2f676b7df
- <you@example.com>
2020-12-27 03:44:18 +0700

    second commit

--- task2.py
+++ task2.py
@@ -9,3 +9,47 @@
 абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
 """
 
+
+class Clothes:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Общая площадь ткани : \n'
+                   f' {(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+
+class Coat(Clothes):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Расход ткани на пальто : {self.square_c}'
+
+
+class Suit(Clothes):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_j = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Расход ткани на костюм : {self.square_j}'
+
+
+coat = Coat(2, 4)
+suit = Suit(1, 2)
+print(coat)
+print(suit)
+print(coat.get_sq_full)
+print(suit.get_sq_full)
+print(suit.get_square_c())
+print(suit.get_square_j())
