9957446f517185e1b9958c64f929d47ea09f5482
- <you@example.com>
2020-12-27 04:14:11 +0700

    second commit

--- task1.py
+++ task1.py
@@ -13,24 +13,23 @@ Matrix (двух матриц). Результатом сложения долж
 
 class Matrix:
     def __init__(self, list_1, list_2):
-        # self.matr = [list_1, list_2]
         self.list_1 = list_1
         self.list_2 = list_2
 
     def __add__(self):
-        matr = [[0, 0, 0],
+        matrix = [[0, 0, 0],
                 [0, 0, 0],
                 [0, 0, 0]]
 
         for i in range(len(self.list_1)):
 
             for j in range(len(self.list_2[i])):
-                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
+                matrix[i][j] = self.list_1[i][j] + self.list_2[i][j]
 
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matrix]))
 
     def __str__(self):
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in Matrix]))
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in Matrix['']]))
 
 
 my_matrix = Matrix([[5, 18, 11],
@@ -39,7 +38,6 @@ my_matrix = Matrix([[5, 18, 11],
                    [[45, 8, 2],
                     [6, 7, 93],
                     [24, 5, 97]])
-# result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
 print(my_matrix.__add__())
