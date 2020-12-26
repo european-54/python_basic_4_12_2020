788974f718a191121287475b534049c31abfcd30
- <you@example.com>
2020-12-27 03:46:23 +0700

    second commit

--- task3.py
+++ task3.py
@@ -52,11 +52,11 @@ class Cell:
         return Cell(round(self.quantity // other.quantity))
 
 
-cells1 = Cell(33)
-cells2 = Cell(9)
+cells1 = Cell(48)
+cells2 = Cell(17)
 print(cells1)
 print(cells1 + cells2)
 print(cells2 - cells1)
-print(cells2.make_order(5))
-print(cells1.make_order(10))
+print(cells2.make_order(3))
+print(cells1.make_order(16))
 print(cells2 / cells1)
